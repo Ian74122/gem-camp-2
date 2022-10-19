888525bb964c9b5fc7157693b25cb547ad71df08
Ian.wu <admin@rails.school>
2022-10-19 10:40:51 +0800

    add edit and update action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,4 +19,17 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :content))
+      redirect_to posts_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
