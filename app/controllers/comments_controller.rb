7039428fa66266b321a7df988aee953d6a5af8a4
Ian.wu <admin@rails.school>
2022-10-20 11:42:33 +0800

    comment edit & update
    refactor form to partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: [:edit, :update]
+
   def index
     @comments = @post.comments
   end
@@ -7,7 +9,6 @@ class CommentsController < ApplicationController
   def new
     @comment = @post.comments.build
   end
-
 
   def create
     @comment = @post.comments.build(params_comment)
@@ -17,7 +18,22 @@ class CommentsController < ApplicationController
       render :new
     end
   end
+
+  def edit ;end
+
+  def update
+    if @comment.update(params_comment)
+      redirect_to post_comments_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
 
   def set_post
     @post = Post.find params[:post_id]
