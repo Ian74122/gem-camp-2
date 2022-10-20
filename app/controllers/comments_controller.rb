286c9636c5eff935a0e023bc341d5c3996303bc0
Ian.wu <admin@rails.school>
2022-10-20 11:19:06 +0800

    comment new & create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,26 @@ class CommentsController < ApplicationController
     @comments = @post.comments
   end
 
+  def new
+    @comment = @post.comments.build
+  end
+
+
+  def create
+    @comment = @post.comments.build(params_comment)
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
   private
 
   def set_post
     @post = Post.find params[:post_id]
+  end
+
+  def params_comment
+    params.require(:comment).permit(:content)
   end
 end
