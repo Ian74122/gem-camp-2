3b776bc9f4a364c930cebc0cdda5dc12a863308d
Ian.wu <admin@rails.school>
2022-10-21 11:46:25 +0800

    rewrite destroy method in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
 class Post < ApplicationRecord
+  default_scope { where(delete_at: nil) }
   validates :title, presence: true
   validates :content, presence: true
 
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+
+  def destroy
+    update(delete_at: Time.now)
+  end
 end
