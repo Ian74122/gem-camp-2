f3db441d5dd42915adc2e8e361c86786687590a9
Ian.wu <admin@rails.school>
2022-10-21 11:37:39 +0800

    add column delete at to post

---
+++ db/migrate/20221021033527_add_delete_at_to_post.rb
@@ -0,0 +1,5 @@
+class AddDeleteAtToPost < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :delete_at, :datetime, default: nil
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_022121) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_033527) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_022121) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "delete_at"
   end
 
 end
