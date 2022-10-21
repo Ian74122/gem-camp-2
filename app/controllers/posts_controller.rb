209be91d6900cd97e4445042d8666e07aa25f5c8
Ian.wu <admin@rails.school>
2022-10-21 11:14:07 +0800

    post can select categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,6 +44,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, category_ids: [])
   end
 end
