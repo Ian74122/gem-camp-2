8115bdf5f47338aaf7c2872aaecf8a2e763e8a0f
Ian.wu <admin@rails.school>
2022-10-24 12:05:26 +0800

    fix N+1 query

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :validate_post_owner, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:categories).all
+    @posts = Post.includes(:categories, :user).all
   end
 
   def new
