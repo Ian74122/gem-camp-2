e733b5090bf6badb905c3e01806007c02eb8e87b
Ian.wu <admin@rails.school>
2022-10-24 11:46:46 +0800

    add user when create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -11,6 +12,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       redirect_to posts_path
     else
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+  belongs_to :user
 
   def destroy
     update(delete_at: Time.now)
