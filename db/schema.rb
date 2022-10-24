4ce7346c2239ef4ecb6d66b31ce6cb546b35be99
Ian.wu <admin@rails.school>
2022-10-24 11:34:45 +0800

    add reference posts to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_025701) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_033256) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_025701) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "delete_at"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
