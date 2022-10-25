25a5e1fb5afcfe281ee8f10b56205cf0946d1e71
Ian.wu <admin@rails.school>
2022-10-25 10:22:28 +0800

    generate migration add user id to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_033256) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_022052) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_033256) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "post_category_ships", force: :cascade do |t|
