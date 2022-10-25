243a40e8e56c96696fdf916dbb72d3b450572e28
Ian.wu <admin@rails.school>
2022-10-25 11:32:31 +0800

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  user = User.create!(email: Faker::Internet.email, password: 'qwer4321', password_confirmation: "qwer4321")
+  puts "create user id: #{user.id}, email: #{user.email}"
+end
+
+30.times do |i|
+  puts "start create #{i} post"
+  post = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user: User.all.sample)
+  (1..20).to_a.sample.times do
+    Comment.create(content: Faker::Lorem.sentence, user: User.all.sample, post: post)
+  end
+  puts "finish #{i} post"
+end
