bdaed3ffb338e92ba036478db91817770fa7a581
Ian.wu <admin@rails.school>
2022-10-25 10:28:57 +0800

    comment belong to user  and done the feature in controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   before_action :set_post
   before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :validate_comment_owner, only: [:edit, :update, :destroy]
 
   def index
     @comments = @post.comments
@@ -12,6 +14,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @post.comments.build(params_comment)
+    @comment.user = current_user
     if @comment.save
       redirect_to post_comments_path(@post)
     else
@@ -45,5 +48,12 @@ class CommentsController < ApplicationController
 
   def params_comment
     params.require(:comment).permit(:content)
+  end
+
+  def validate_comment_owner
+    unless @comment.user == current_user
+      flash[:notice] = 'the comment not belongs to you'
+      redirect_to post_comments_path(@post)
+    end
   end
 end
